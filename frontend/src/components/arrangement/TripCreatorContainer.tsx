d44ed2207a567ea717522cfdb9649065aeb40710
DESKTOP-J7QTMBR\mikke <mikkel.marsteinvikenfiber.no>
2022-02-10 20:39:29 +0100

    #1 functional tripcreator interface

--- frontend/src/components/arrangement/TripCreatorContainer.tsx
+++ frontend/src/components/arrangement/TripCreatorContainer.tsx
@@ -1,6 +1,10 @@
-import React, { FunctionComponent, useState } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
-import DatePicker from "react-datepicker";
+import React, { FunctionComponent } from 'react';
+import { Form, Button, Row } from 'react-bootstrap';
+import TripCreatorDatePicker from './TripCreatorDatePicker';
+import { TripCreatorDifficulty } from './TripCreatorDifficulty';
+import { TripCreatorName } from './TripCreatorName';
+import TripCreatorDescription from './TripCreatorDescription';
+import { TripCreatorWhere } from './TripCreatorWhere';
 
 type IProps = {
   onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
@@ -8,37 +12,19 @@ type IProps = {
 
 export const TripCreatorContainer: FunctionComponent<IProps> = ({ 
 }) => {  
-  const DiffList: string[] = ["Lett", "Moderat", "Vanskelig"]
-  const [startDate, setStartDate] = useState(new Date());
-  return <Form className="TripCreatorContainer">
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-  <Row>
-    <Col>
-    <Form.Label>Navn på turen</Form.Label>
-    </Col>
-    <Col>
-    <Form.Control type="string" placeholder="Navn..." />
-    </Col>
-  </Row>
-  
-  </Form.Group>
-  <Row>
-    <Form.Label>Deskripsjon</Form.Label>
-    <Form.Control type="string" placeholder="En fin tur langs..." />
-  
-    <Form.Label>Når?</Form.Label>
-    <DatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)} />
 
-    <Form.Label>Vansklighetsgrad</Form.Label>
-    <Form.Select aria-label="Default select example">
-      {DiffList.map((data: string) => <option value={data}>{data}</option>)}
-    </Form.Select>
+  return <Form className="TripCreatorContainer">
+    <TripCreatorName/>
+    <Row>
+      <TripCreatorDescription/>
+      <TripCreatorDatePicker/>
+      <TripCreatorWhere/>
+      <TripCreatorDifficulty/>
     </Row>
     <Row>
-      
-  <Button variant="primary" type="submit" className="TripCreatorContainer__SubmitButton">
-    Submit
-  </Button>
-  </Row>
+      <Button variant="primary" type="submit" className="TripCreatorContainer__SubmitButton">
+        Submit
+      </Button>
+    </Row>
 </Form>
 }
---
+++ frontend/src/components/arrangement/TripCreatorName.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
+
+type IProps = {}
+export const TripCreatorName: FunctionComponent<IProps> = ({ 
+}) => {  
+  return <Row>
+    <Col>
+    <Form.Label>Navn på turen</Form.Label>
+    </Col>
+    <Col>
+    <Form.Control type="string" placeholder="Navn..." />
+    </Col>
+  </Row>
+}
