38448c6516447880908ac82abd05da2a58ec9cf6
DESKTOP-J7QTMBR\mikke <mikkel.marsteinvikenfiber.no>
2022-02-11 11:31:22 +0100

    #17 Basic working router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { TripCreatorContainer } from './components/arrangement/TripCreatorContainer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TripCreatorContainer/>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/newTrip">Create new trip</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <Routes>
+            <Route path="/" element={Home()} />
+            <Route path="/newTrip" element={<TripCreatorContainer />} />
+            <Route path="/about" element={About()} />
+          </Routes >
+        </Router>
       </header>
-      
+
+
     </div>
   );
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
