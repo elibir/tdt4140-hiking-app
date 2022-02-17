286c2ae71d0df7dfe2f5829d27c9475dc0476850
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-02-17 14:49:04 +0100

    #9 added correct fields for events

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -5,7 +5,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-
+ 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
@@ -14,3 +14,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
+    
