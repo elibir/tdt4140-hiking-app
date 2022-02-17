a8793e2dfb4c4d1a8334d221d82aff0bf8fd7a70
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-02-17 15:17:51 +0100

    #9 serializer change test

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -3,33 +3,8 @@ from events.models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=30)
-    description = serializers.CharField(max_length=100)
-    where = serializers.CharField(max_length=30)
-    date_time = serializers.DateField(null=True, blank=True)
-    difficulty = serializers.PositiveSmallIntegerField(choices=((1, "Lett"), (2, "Moderat"), (3, "Vanskelig")))
-    created_at = serializers.DateTimeField(default=now, editable=False)
-
+    
     #owner = serializers.ReadOnlyField(source='owner.username') 
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Event` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Event` instance, given the validated data.
-        """
-        instance.name = validated_data.get('name', instance.title)
-        instance.description = validated_data.get('code', instance.code)
-        instance.where = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
 
     class Meta:
         model = Event
