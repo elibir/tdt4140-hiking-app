fd88c585c6f169e4aaca34aebe61f854da4df97c
Aurorako <aurorako@stud.ntnu.no>
2022-02-24 13:47:47 +0100

    #12 Opprettet EventPage

---
+++ frontend/src/components/event_pages/EventPage.tsx
@@ -0,0 +1,16 @@
+import React, {FunctionComponent} from "react"
+import { Col, Container, Row } from "react-bootstrap"
+type IProps = {
+
+}
+export const EventPage: FunctionComponent<IProps> = ({
+}) => {
+    return (
+        <Container>
+            <Row>Navn</Row>
+            <Row>---</Row>
+            <Row>---</Row>
+        </Container>
+    )
+
+}
