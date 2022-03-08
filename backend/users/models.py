2300e9357aa2ba1b4cd29e4f1f311bd81c4563d3
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-03-08 14:20:47 +0100

    setup one-to-many realtion between user and events

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,27 @@
 from django.db import models
-
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    events_created = None;
-    events_registrated = None;
-
+from django.contrib.auth.models import User
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from events.models import Event
 # Create your models here.
+class ExtendedUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    events_created = models.OneToManyField( #one-to-many relation, one user can created several events
+        Event, 
+        through='Creator',
+        through_fields=('event', 'user'),
+        )
+
+
+#model to connect User with Event
+class Creator:
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
+
+    
+
+
+
 
 
 
