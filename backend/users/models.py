409eee2eaa7624a133d663e6eb00b11ea9fa0741
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-03-08 14:37:37 +0100

    setup participating users and events

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,18 +6,27 @@ from events.models import Event
 # Create your models here.
 class ExtendedUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    events_created = models.OneToManyField( #one-to-many relation, one user can created several events
+    events_owning = models.OneToManyField( #one-to-many relation, one user can created several events
         Event, 
-        through='Creator',
+        through='Owner',
         through_fields=('event', 'user'),
         )
+    events_participating = models.ManyToManyField(
+        Event, 
+        through='Participator',
+        through_fields=('event', 'user'),
+    )
     
     def __str__(self):
         return self.user.username;
 
 
-#model to connect User with Event
-class Creator:
+#model to connect User with Event 
+class Owner:
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
+
+class Participator:
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
 
