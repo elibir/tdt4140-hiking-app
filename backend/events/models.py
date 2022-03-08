7e64dad08598459f8f84839172903cb91a2ff0ce
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-03-08 14:51:15 +0100

    more fitting to move to event model

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -10,12 +12,32 @@ class Event(models.Model):
     date_time = models.DateField(null=True, blank=True)
     difficulty = models.PositiveSmallIntegerField(choices=((1, "Lett"), (2, "Moderat"), (3, "Vanskelig")))
     created_at = models.DateTimeField(default=now, editable=False, null=True)
-    user = models.ForeignKey('auth.User', related_name="events", on_delete=models.CASCADE, null=True)
+    #user = models.ForeignKey('auth.User', related_name="events", on_delete=models.CASCADE, null=True)
     capacity = models.IntegerField(null=True)
     #participants = models.ManyToManyField('auth.User', related_name="events", on_delete=models.CASCADE)
 
+    owner = models.ManyToOneRel(
+        User,
+        through='Owner',
+        through_fields=('event', 'user'),
+    )
+
+    participants = models.ManyToManyField(
+        User,
+        through='Participant',
+        through_fields=('event', 'user'),
+    )
+
     def __str__(self):
         return self.name
+    
+class Owner:
+    event = model.models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = model.models.ForeignKey(User, on_delete=models.CASCADE) 
+
+class Participator:
+    event = model.models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = model.models.ForeignKey(User, on_delete=models.CASCADE) 
 
     #def save(self):
        # if not self.participants.all():
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,37 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.base import Model
-from django.db.models.deletion import CASCADE
-from events.models import Event
 # Create your models here.
-class ExtendedUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    events_owning = models.OneToManyField( #one-to-many relation, one user can created several events
-        Event, 
-        through='Owner',
-        through_fields=('event', 'user'),
-        )
-    events_participating = models.ManyToManyField(
-        Event, 
-        through='Participator',
-        through_fields=('event', 'user'),
-    )
-    
-    def __str__(self):
-        return self.user.username;
-
-
-#model to connect User with Event 
-class Owner:
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
-
-class Participator:
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
-
-    
-
 
 
 
