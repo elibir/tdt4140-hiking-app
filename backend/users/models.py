b539e419276e4f583dffe1046d94a6811f9a4101
Jakob Tøssesbro <jakobto@stud.ntnu.no>
2022-03-08 13:19:24 +0100

    extended user model to store event info

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    events_created = None;
+    events_registrated = None;
 
 # Create your models here.
 
