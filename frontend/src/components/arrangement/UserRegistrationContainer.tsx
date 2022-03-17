e3650d3c19e6e64ec5b7d89479c578591bd95f4c
sarmip <sarmip@stud.ntnu.no>
2022-03-17 14:19:06 +0100

    #65 La til tekst på opprett bruker siden

--- frontend/src/components/arrangement/UserRegistrationContainer.tsx
+++ frontend/src/components/arrangement/UserRegistrationContainer.tsx
@@ -1,12 +1,11 @@
 import React, { FunctionComponent, useState } from 'react';
 import { Form, Button, Row, Col, Container, CardGroup, Card } from 'react-bootstrap';
-import './RegistrationContainer.css'; //endre tilbake
+import './UserRegistrationContainer.css';
 
 import { sendData } from '../../utils/APIUtils';
 import { handleLogin } from '../../Helper';
 import { useNavigate } from "react-router-dom";
-import user from "../images/user.png";
-import building from "../images/building.png";
+
 
 type IProps = {
   onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
@@ -32,81 +31,49 @@ export const UserRegistrationContainer: FunctionComponent<IProps> = ({
     navigate("/login");
   }
   return (
-    <Container className="RegistrationContainer">
-      <Row md={2} className="userCardRow">
-        <Col>
-          <Card className="userCard" >
-
-            <Card.Body>
-              <Card.Img variant="top" style={{ height: "320px", width: "280px", alignContent: "center" }} src={user} />
 
 
+    <Container className="UserRegistrationContainer">
+      <Form onSubmit={(e) => onFormSubmit(e)}>
 
-            </Card.Body>
-            <Card.Title className="RegText">VANLIG BRUKER</Card.Title>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="userCard">
+        <Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label >Fornavn</Form.Label>
+            <Form.Control name="first_name" type="string" placeholder="Fornavn" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label >Etternavn</Form.Label>
+            <Form.Control name="last_name" type="string" placeholder="Etternavn" />
+          </Form.Group>
+        </Row>
 
-            <Card.Body >
-              <Card.Img variant="top" src={building} />
+        <Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label >E-post</Form.Label>
+            <Form.Control name="email" type="email" placeholder="E-post" />
+          </Form.Group>
+        </Row>
 
+        <Row>
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label >Brukernavn</Form.Label>
+            <Form.Control name="username" type="string" placeholder="Brukernavn" />
+          </Form.Group>
+        </Row>
 
+        <Row>
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label >Passord</Form.Label>
+            <Form.Control name="password" type="string" placeholder="Passord" />
+          </Form.Group>
+        </Row>
 
-            </Card.Body>
-            <Card.Title className="RegText">BEDRIFTSBRUKER</Card.Title>
+        <Row>
+          <Button variant="primary" type="submit" className="UserRegistration_SubmitButton">Opprett bruker</Button>
+        </Row>
 
-          </Card>
-
-        </Col>
-      </Row>
-
-
-
+      </Form>
     </Container>
-
-    // <Container className="UserRegistrationContainer">
-    //   <Form onSubmit={(e) => onFormSubmit(e)}>
-
-    //     <Row>
-    //       <Form.Group as={Col} controlId="formGridEmail">
-    //         <Form.Label >Fornavn</Form.Label>
-    //         <Form.Control name="first_name" type="string" placeholder="Fornavn" />
-    //       </Form.Group>
-    //       <Form.Group as={Col} controlId="formGridEmail">
-    //         <Form.Label >Etternavn</Form.Label>
-    //         <Form.Control name="last_name" type="string" placeholder="Etternavn" />
-    //       </Form.Group>
-    //     </Row>
-
-    //     <Row>
-    //       <Form.Group as={Col} controlId="formGridEmail">
-    //         <Form.Label >E-post</Form.Label>
-    //         <Form.Control name="email" type="email" placeholder="E-post" />
-    //       </Form.Group>
-    //     </Row>
-
-    //     <Row>
-    //       <Form.Group as={Col} controlId="formGridPassword">
-    //         <Form.Label >Brukernavn</Form.Label>
-    //         <Form.Control name="username" type="string" placeholder="Brukernavn" />
-    //       </Form.Group>
-    //     </Row>
-
-    //     <Row>
-    //       <Form.Group as={Col} controlId="formGridPassword">
-    //         <Form.Label >Passord</Form.Label>
-    //         <Form.Control name="password" type="string" placeholder="Passord" />
-    //       </Form.Group>
-    //     </Row>
-
-    //     <Row>
-    //       <Button variant="primary" type="submit" className="UserRegistration_SubmitButton">Opprett bruker</Button>
-    //     </Row>
-
-    //   </Form>
-    // </Container>
   )
 
 }
